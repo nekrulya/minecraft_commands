29750c63230ea493fb73fdbaf327701ddb29a8c1
nekrulya <nekrulyaa@mail.ru>
2024-10-21 16:47:14 +0500

    тест

--- src/main.py
+++ src/main.py
@@ -40,6 +40,7 @@ app.add_middleware(
 async def root(request: Request):
     return RedirectResponse(url="/docs", status_code=HTTP_301_MOVED_PERMANENTLY)
 
+print('hello')
 
 app.include_router(router_auth)
 
